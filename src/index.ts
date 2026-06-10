07d2e8ce12df93cc5d8ec789d6727449efd14c65
Divyanshu Shekhar <divyanshushekhar987@gmail.com>
2026-06-10 11:50:02 +0530

    🐙 : barrel export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export { LineChart } from "./rn";
-export { GenerateStringPath, GetYForX, FindDomain } from "./math";
-export { CandleStickChart } from "./candlestick";
+// Barrel File
+
+// Export components
+
+export {default as CandleStickChart} from "./CandleStickChart"
+export {default as HorizontalBarChart} from "./HorizontalBarChart"
+export {default as VerticalBarChart} from "./VerticalBarChart"
+export {default as PieChart} from "./PieChart"
+export {default as LineChart} from "./LineChart"
+export {default as TimeSeriesChart} from "./TimeSeriesChart"
+
+// Export types
+export * from "./CandleStickChart/types"
+export * from "./HorizontalBarChart/types"
+export * from "./VerticalBarChart/types"
+export * from "./PieChart/types"
+export * from "./LineChart/types"
+export * from "./TimeSeriesChart/types"
+
